7a4edd345ada9414de288ba8678b4f07acd4fe0c
LaptevaKate <ekaterina213@yandex.ru>
2022-04-24 12:44:50 +0300

    first commit

---
+++ HomeWork13/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork13
+//
+//  Created by Екатерина Лаптева on 24.04.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
